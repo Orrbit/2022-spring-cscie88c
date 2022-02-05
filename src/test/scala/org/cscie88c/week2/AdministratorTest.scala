45e5f9ddc4cf9968efe0007c98871533f2444b26
Liam Orr <orrbit76@gmail.com>
2022-02-05 13:00:37 -0500

    add admin and faculty subclasses

--- src/test/scala/org/cscie88c/week2/AdministratorTest.scala
+++ src/test/scala/org/cscie88c/week2/AdministratorTest.scala
@@ -4,5 +4,12 @@ import org.cscie88c.testutils.{StandardTest}
 
 // write unit tests for Administrator below
 class AdministratorTest extends StandardTest {
-  
+  "Administrator" when {
+    "instantiated" should {
+      "have a huge budget for Tom Brady" in {
+        val admin = new Administrator("Tom Brady", "tbrady@gmail.com", 23000000)
+        admin.description.contains("$23000000") shouldBe true
+      }
+    }
+  }
 }
--- src/test/scala/org/cscie88c/week2/FacultyTest.scala
+++ src/test/scala/org/cscie88c/week2/FacultyTest.scala
@@ -4,5 +4,12 @@ import org.cscie88c.testutils.{StandardTest}
 
 // write unit tests for Faculty below
 class FacultyTest extends StandardTest {
-  
+  "Faculty" when {
+    "instantiated" should {
+      "have a course id property for Ms. Frizzle" in {
+        val fac = new Faculty("Ms. Frizzle", "mfrizzle@gmail.com", "1-sci")
+        fac.description.contains("1-sci") shouldBe true
+      }
+    }
+  }
 }
