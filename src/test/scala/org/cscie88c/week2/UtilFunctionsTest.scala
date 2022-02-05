09c620607b4a0035edff0771992c4d426dc28d9a
Liam Orr <orrbit76@gmail.com>
2022-02-05 10:52:20 -0500

    add util functions for maximum and average

--- src/test/scala/org/cscie88c/week2/UtilFunctionsTest.scala
+++ src/test/scala/org/cscie88c/week2/UtilFunctionsTest.scala
@@ -9,10 +9,25 @@ class UtilFunctionsTest extends StandardTest {
       "return maximum of two ints when first integer is greater" in {
         UtilFunctions.maximum(2, 1) should be (2)
       }
-      // add more unit tests for maximum below
+      "return maximum of two ints when second integer is greater" in {
+        UtilFunctions.maximum(-5, -2) should be (-2)
+      }
+    }
+    "average" should {
+      "return average of two ints when both are positive" in {
+        UtilFunctions.average(3, 1) should be (2)
+      }
+      "return average of two ints when both are negative" in {
+        UtilFunctions.average(-15, -5) should be (-10)
+      }
+      "return average of two ints when one is negative and one is positive" in {
+        UtilFunctions.average(-4, 50) should be (23)
+      }
+      "return average when result is not a whole number" in {
+        UtilFunctions.average(1, 2) should be (1.5)
+      }
     }
 
-    // add unit tests for average below
-    
+
   }
 }
