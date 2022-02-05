fddfd3a39a1d234e3ad4aa9e4bf65945f10e351f
Liam Orr <orrbit76@gmail.com>
2022-02-05 10:53:30 -0500

    add university employee class and test

--- src/main/scala/org/cscie88c/week2/UniversityEmployee.scala
+++ src/main/scala/org/cscie88c/week2/UniversityEmployee.scala
@@ -1,3 +1,6 @@
 package org.cscie88c.week2
 
 // write the class UniversityEmployee below
+class UniversityEmployee(name:String, email:String){
+    def description: String = s"Name: ${name}, Email: ${email}"
+}
--- src/test/scala/org/cscie88c/week2/UniversityEmployeeTest.scala
+++ src/test/scala/org/cscie88c/week2/UniversityEmployeeTest.scala
@@ -6,8 +6,24 @@ import org.cscie88c.testutils.{StandardTest}
 class UniversityEmployeeTest extends StandardTest {
   "UniversityEmployee" when {
     "instantiated" should {
-      "have a name property" in {
-        true shouldBe true
+      "have a name property for Bruce Lee" in {
+        val emp = new UniversityEmployee("Bruce Lee", "brucel@gmail.com")
+        emp.description.contains("Bruce Lee") shouldBe true
+      }
+
+      "have an email property for Bruce Lee" in {
+        val emp = new UniversityEmployee("Bruce Lee", "brucel@gmail.com")
+        emp.description.contains("brucel@gmail.com") shouldBe true
+      }
+
+      "have a name property for Tom Cruise" in {
+        val emp = new UniversityEmployee("Tom Cruise", "tomc@gmail.com")
+        emp.description.contains("Tom Cruise") shouldBe true
+      }
+
+      "have an email property for Tom Cruise" in {
+        val emp = new UniversityEmployee("Tom Cruise", "tomc@gmail.com")
+        emp.description.contains("tomc@gmail.com") shouldBe true
       }
     }
   }
