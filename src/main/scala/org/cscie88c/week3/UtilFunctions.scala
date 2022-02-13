4e46a87f117d056075fc3c5de815d8ebb15032fa
Liam Orr <orrbit76@gmail.com>
2022-02-13 12:47:57 -0500

    demonstrate for comprehensions with pythagorean triplets

--- src/main/scala/org/cscie88c/week3/UtilFunctions.scala
+++ src/main/scala/org/cscie88c/week3/UtilFunctions.scala
@@ -4,8 +4,13 @@ object UtilFunctions {
   
   def mult2(x: Int, y: Int): Int = x * y
   
-  def pythTest(x: Int, y: Int, z: Int): Boolean = ???
+  def pythTest(x: Int, y: Int, z: Int): Boolean = (x*x + y*y) == z*z
 
-  val pythTriplesUpto100: List[(Int, Int, Int)] = ???
+  val pythTriplesUpto100: List[(Int, Int, Int)] = for {
+    x <- (1 to 100).toList
+    y <- (1 to 100).toList
+    z <- (1 to 100).toList
+    if pythTest(x,y,z)
+  } yield (x,y,z)
 
 }
--- src/test/scala/org/cscie88c/week3/UtilFunctionsTest.scala
+++ src/test/scala/org/cscie88c/week3/UtilFunctionsTest.scala
@@ -5,8 +5,19 @@ import org.cscie88c.testutils.{ StandardTest }
 class UtilFunctionsTest extends StandardTest {
   "UtilFunctions" when {
     "with pythTriplesUpto100" should {
-      "verify elements in list are pythagorean triples" in {
-        // write your test here
+      "verify first element in list is a pythagorean triple" in {
+        val e1 = UtilFunctions.pythTriplesUpto100.head
+        UtilFunctions.pythTest(e1._1, e1._2, e1._3) should be (true)
+      }
+
+      "verify second element in list is a pythagorean triple" in {
+        val e2 = UtilFunctions.pythTriplesUpto100.tail.head
+        UtilFunctions.pythTest(e2._1, e2._2, e2._3) should be (true)
+      }
+
+      "verify third element in list is a pythagorean triple" in {
+        val e3 = UtilFunctions.pythTriplesUpto100.tail.tail.head
+        UtilFunctions.pythTest(e3._1, e3._2, e3._3) should be (true)
       }
     }
   }
