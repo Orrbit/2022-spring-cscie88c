57f02f135b4dc0442000eb6f15b0a0617c3500b4
Liam Orr <orrbit76@gmail.com>
2022-02-13 12:48:33 -0500

    WordSpec unit tests with Student class

--- src/main/scala/org/cscie88c/week3/Student.scala
+++ src/main/scala/org/cscie88c/week3/Student.scala
@@ -12,15 +12,28 @@ final case class Student(
 
 object Student {
 
-  def validateEmail(student: Student): Boolean = ???
+  def validateEmail(student: Student): Boolean = student.email.contains('@')
 
   def averageScoreBySubject(
       subject: String,
       studentList: List[Student]
-    ): Double = ???
+    ): Double = {
+      val studentScoresInSubject = studentList.filter(s => s.subject.equals(subject)).map(s => s.score)
+      if (studentScoresInSubject.length != 0) 
+        studentScoresInSubject.sum.toDouble / studentScoresInSubject.length.toDouble
+      else
+        0
+    }
 
   def averageScoreByStudent(
       student: Student,
       studentList: List[Student]
-    ): Double = ???
+    ): Double = {
+      val scoresOfStudents = studentList.filter(s => s.name.equals(student.name)).map(s => s.score)
+      scoresOfStudents.sum.toDouble / scoresOfStudents.length.toDouble
+      if (scoresOfStudents.length != 0) 
+        scoresOfStudents.sum.toDouble / scoresOfStudents.length.toDouble
+      else
+        0
+    }
 }
