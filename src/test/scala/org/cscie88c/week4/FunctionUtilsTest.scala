47057a4e0f07a37adcdc42ed55913ab09f00cbaa
Liam Orr <orrbit76@gmail.com>
2022-02-19 15:54:50 -0500

    format project

--- src/test/scala/org/cscie88c/week4/FunctionUtilsTest.scala
+++ src/test/scala/org/cscie88c/week4/FunctionUtilsTest.scala
@@ -1,18 +1,18 @@
 package org.cscie88c.week4
 
-import org.cscie88c.testutils.{StandardTest}
+import org.cscie88c.testutils.{ StandardTest }
 
 class FunctionUtilsTest extends StandardTest {
 
-   "FunctionUtils" when {
+  "FunctionUtils" when {
     "calling applyNtimes" should {
       "return the correct value" in {
         // write unit test here
       }
-    
+
     }
 
     // write unit tests for other functions here
   }
- 
+
 }
--- src/test/scala/org/cscie88c/week4/ListUtilsTest.scala
+++ src/test/scala/org/cscie88c/week4/ListUtilsTest.scala
@@ -1,6 +1,6 @@
 package org.cscie88c.week4
 
-import org.cscie88c.testutils.{StandardTest}
+import org.cscie88c.testutils.{ StandardTest }
 
 class ListUtilsTest extends StandardTest {
   "ListUtils" when {
@@ -8,7 +8,7 @@ class ListUtilsTest extends StandardTest {
       "return the correct value" in {
         // write unit test here
       }
-    
+
     }
 
     // write unit tests for zeroes, and other functions here
