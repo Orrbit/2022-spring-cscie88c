153a1e657bce0f8aed4f1dc0a4ded9b43d544c80
Liam Orr <orrbit76@gmail.com>
2022-02-20 12:29:49 -0500

    completing function utils to display depth of higher order function knowledge

--- src/main/scala/org/cscie88c/week4/FunctionUtils.scala
+++ src/main/scala/org/cscie88c/week4/FunctionUtils.scala
@@ -3,9 +3,14 @@ package org.cscie88c.week4
 object FunctionUtils {
 
   // complete the implementation of the higher order functions below
-  def applyNtimes(n: Int)(x: Int)(f: Int => Int): Int = ???
+  def applyNtimes(n: Int)(x: Int)(f: Int => Int): Int =
+    (1 to n).fold(x)((acc: Int, _) => f(acc))
 
-  def myPositivePower(x: Int, n: Int): Int = ???
+  def myPositivePower(x: Int, n: Int): Int =
+    applyNtimes(n)(1)((num: Int) => num * x)
 
-  def deferredExecutor(name: String)(f: Int => Int): Int => Int = ???
+  def deferredExecutor(name: String)(f: Int => Int): Int => Int = (x: Int) => {
+    print(s"running on deferred executor ${name} with value ${x}")
+    f(x)
+  }
 }
--- src/test/scala/org/cscie88c/week4/FunctionUtilsTest.scala
+++ src/test/scala/org/cscie88c/week4/FunctionUtilsTest.scala
@@ -5,14 +5,32 @@ import org.cscie88c.testutils.{ StandardTest }
 class FunctionUtilsTest extends StandardTest {
 
   "FunctionUtils" when {
+
+    def add5(x: Int) = x + 5
+
     "calling applyNtimes" should {
-      "return the correct value" in {
-        // write unit test here
+
+      "return the correct value on init value 0, 3 times" in {
+        FunctionUtils.applyNtimes(3)(0)(add5) should be(15)
+      }
+
+      "return the correct value on init value 20, 3 times" in {
+        FunctionUtils.applyNtimes(3)(20)(add5) should be(35)
+      }
+
+      "return the correct value on init value 5, 10 times" in {
+        FunctionUtils.applyNtimes(10)(5)(add5) should be(55)
       }
 
     }
 
-    // write unit tests for other functions here
+    "calling deferredExecutor" should {
+
+      "return the correct value" in {
+        FunctionUtils.deferredExecutor("CPU Pool")(add5)(4) should be(9)
+      }
+
+    }
   }
 
 }
