68991e7355203c176c856a1383086da8236776b8
Liam Orr <orrbit76@gmail.com>
2022-04-03 08:25:04 -0400

    add a rdd app to process all transactions in a csv dataset and group by year

--- src/main/scala/org/cscie88c/week10/SparkRDDApplication.scala
+++ src/main/scala/org/cscie88c/week10/SparkRDDApplication.scala
@@ -1,36 +1,68 @@
 package org.cscie88c.week10
 
 import org.apache.spark.sql.SparkSession
-import com.typesafe.scalalogging.{LazyLogging}
-import org.cscie88c.config.{ConfigUtils}
-import org.cscie88c.utils.{SparkUtils}
-import org.apache.spark.rdd.{RDD}
+import com.typesafe.scalalogging.{ LazyLogging }
+import org.cscie88c.config.{ ConfigUtils }
+import org.cscie88c.utils.{ SparkUtils }
+import org.apache.spark.rdd.{ PairRDDFunctions, RDD }
 import pureconfig.generic.auto._
+import org.cscie88c.week10.CustomerTransaction
+import java.time.format.DateTimeFormatter
 
-// write case class below
-// case class SparkRDDConfig()
+case class SparkRDDConfig(
+    name: String,
+    masterUrl: String,
+    transactionFile: String
+  )
 
 // run with: sbt "runMain org.cscie88c.week10.SparkRDDApplication"
 object SparkRDDApplication {
 
+  val APP_CONFIG_PATH = "org.cscie88c.spark-rdd-application"
+
   // application entry point
   def main(args: Array[String]): Unit = {
-    implicit val conf:SparkRDDConfig = readConfig()                         // 1. read configuration
-    val spark = SparkUtils.sparkSession(conf.name, conf.masterUrl)          // 2. initialize spark session
-    val rddLines = loadData(spark)                                          // 3.load data
-    val rddTransactions = lineToTransactions(rddLines)                      // 4. convert lines to transaction objects
-    val yearlyTransactionsRDD = transactionsAmountsByYear(rddTransactions)  // 5. transform data
-    printTransactionsAmountsByYear(yearlyTransactionsRDD)                   // 6. print results
-    spark.stop()                                                            // 7. stop spark cluster
+    implicit val conf: SparkRDDConfig = readConfig()
+    val spark = SparkUtils.sparkSession(
+      conf.name,
+      conf.masterUrl
+    )
+    val rddLines = loadData(spark)
+    val rddTransactions = lineToTransactions(
+      rddLines
+    )
+    val yearlyTransactionsRDD = transactionsAmountsByYear(
+      rddTransactions
+    )
+    printTransactionsAmountsByYear(yearlyTransactionsRDD)
+    spark.stop()
   }
 
-  def readConfig(): SparkRDDConfig = ???
+  def readConfig(): SparkRDDConfig =
+    ConfigUtils.loadAppConfig[SparkRDDConfig](APP_CONFIG_PATH)
 
-  def loadData(spark: SparkSession)(implicit conf: SparkRDDConfig): RDD[String] = ???
+  def loadData(
+      spark: SparkSession
+    )(implicit
+      conf: SparkRDDConfig
+    ): RDD[String] = spark.sparkContext.textFile(conf.transactionFile)
 
-  def lineToTransactions(lines: RDD[String]): RDD[CustomerTransaction] = ???
+  def lineToTransactions(lines: RDD[String]): RDD[CustomerTransaction] =
+    lines.map(CustomerTransaction(_)).collect {
+      case Some(n: CustomerTransaction) => n
+    }
 
-  def transactionsAmountsByYear(transactions: RDD[CustomerTransaction]): RDD[(String, Double)] = ???
+  def transactionsAmountsByYear(
+      transactions: RDD[CustomerTransaction]
+    ): RDD[(String, Double)] = {
+    val get_year_of_date: String => String = s =>
+      s.slice(s.lastIndexOf("-") + 1, s.size)
+    transactions
+      .map(f => (get_year_of_date(f.transactionDate), f.transactionAmount))
+      .reduceByKey((a, b) => a + b)
+  }
 
-  def printTransactionsAmountsByYear(transactions: RDD[(String, Double)]): Unit = ???
+  def printTransactionsAmountsByYear(
+      transactions: RDD[(String, Double)]
+    ): Unit = transactions.foreach(println)
 }
